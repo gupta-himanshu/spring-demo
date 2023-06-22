7cdfcbd60dc86ece4fb713fd81978533fa0ccb2e
Himanshu Gupta <x_himanshu@flow.io>
2023-06-22 07:29:01 -0400

    default profile

--- src/test/java/core/spring/demo/controllers/i18n/Myi18NControllerTestEN.java
+++ src/test/java/core/spring/demo/controllers/i18n/Myi18NControllerTestEN.java
@@ -3,9 +3,7 @@ package core.spring.demo.controllers.i18n;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("EN")
 @SpringBootTest
 class Myi18NControllerTestEN {
 
