0d2d263e2ea160539da9f1cb1d7205248fe96f9a
Himanshu Gupta <x_himanshu@flow.io>
2023-06-22 07:25:43 -0400

    spring profiles

---
+++ src/main/java/core/spring/demo/services/i18n/EnglishGreetingService.java
@@ -0,0 +1,15 @@
+package core.spring.demo.services.i18n;
+
+import core.spring.demo.services.GreetingService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("EN")
+@Service("i18NService")
+public class EnglishGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello World - EN";
+    }
+}
---
+++ src/main/java/core/spring/demo/services/i18n/SpanishGreetingService.java
@@ -0,0 +1,15 @@
+package core.spring.demo.services.i18n;
+
+import core.spring.demo.services.GreetingService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("ES")
+@Service("i18NService")
+public class SpanishGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hola Mundo - ES";
+    }
+}
